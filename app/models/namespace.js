db22049b1727d917305422873f2d48e64181a3a6
Tom Gehrke <phyrog@cognitive-coding.com>
2016-09-12 20:42:05 +0200

    Add namespace

--- app/models/namespace.js
+++ app/models/namespace.js
@@ -3,6 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   slug: DS.attr('string'),
-  description: DS.attr('string'),
-  namespace: DS.belongsTo()
+  repositories: DS.hasMany()
 });
