b813b2c337e00a3601e267ddac1e37cd5b7f5a82
Tom Gehrke <phyrog@cognitive-coding.com>
2016-09-13 11:16:11 +0200

    Create model tests

--- tests/unit/models/repository-test.js
+++ tests/unit/models/repository-test.js
@@ -1,18 +1,30 @@
 import { expect } from 'chai';
 import { describeModel, it } from 'ember-mocha';
+import Ember from 'ember';
 
 describeModel(
   'repository',
   'Unit | Model | repository',
   {
-    // Specify the other units that are required for this test.
-    needs: []
+    needs: ['model:namespace']
   },
   function() {
-    // Replace this with your real tests.
     it('has a name', function() {
-      let model = server.createList('repository', 1);
-      console.log(model);
+      let model = this.subject({ name: 'Foobar' });
+      expect(model.get('name')).to.equal('Foobar');
+    });
+
+    it('has a description', function() {
+      let model = this.subject({ description: 'Some description' });
+      expect(model.get('description')).to.equal('Some description');
+    });
+
+    it('belongs to a namespace', function() {
+      let model = this.store().modelFor('repository');
+      let relationship = Ember.get(model, 'relationshipsByName')
+        .get('namespace');
+      expect(relationship.key).to.equal('namespace');
+      expect(relationship.kind).to.equal('belongsTo');
     });
   }
 );
