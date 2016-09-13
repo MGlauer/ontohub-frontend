c5eed340ad3f7f308e4ec5c8b9502f360d178a32
Tom Gehrke <phyrog@cognitive-coding.com>
2016-09-13 11:16:29 +0200

    Fix lint tests

--- app/routes/namespace/index.js
+++ app/routes/namespace/index.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params) {
+  model() {
     let namespace = this.modelFor('namespace');
     return namespace.get('repositories');
   }
--- app/routes/namespace/repository.js
+++ app/routes/namespace/repository.js
@@ -1,8 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params) {
-    let namespace = this.modelFor('namespace');
-    return namespace.repositories;
-  }
 });
