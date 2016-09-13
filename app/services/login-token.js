68b5019c799e0d0cc4f8fe9e5c8a10d5cd74c676
Tom Gehrke <phyrog@cognitive-coding.com>
2016-09-13 15:16:25 +0200

    Mark args as unused

--- app/services/login-token.js
+++ app/services/login-token.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   token: Ember.computed({
-    get(key) {
+    get(_key) {
       return localStorage.getItem('login-token');
     },
-    set(key, value) {
+    set(_key, value) {
       localStorage.setItem('login-token', value);
       return value;
     }
