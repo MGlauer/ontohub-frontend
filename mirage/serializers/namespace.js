ab44613e6f5821680ee8da06fe0efddfaeb7d2e8
Tom Gehrke <phyrog@cognitive-coding.com>
2016-12-02 13:34:33 +0100

    Remove mirage related files

--- mirage/serializers/namespace.js
+++
@@ -1,5 +0,0 @@
-import Serializer from './application';
-
-export default Serializer.extend({
-  include: ['repositories']
-});
--- tests/helpers/destroy-app.js
+++ tests/helpers/destroy-app.js
@@ -1,7 +1,5 @@
 import Ember from 'ember';
-import { server } from 'ember-cli-mirage';
 
 export default function destroyApp(application) {
   Ember.run(application, 'destroy');
-  server.shutdown();
 }
